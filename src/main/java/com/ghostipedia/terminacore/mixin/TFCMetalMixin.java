460416eaa1b945926d1fa37c61bd96f3d0c80bc6
YoungOnion <39562198+YoungOnionMC@users.noreply.github.com>
2024-08-23 23:43:22 -0600

    Fluid Temps (#6)
    * start of tfc metals
    
    * Fluid Temperatures
    
    * Fix Offset

--- src/main/java/com/ghostipedia/terminacore/mixin/TFCMetalMixin.java
+++ src/main/java/com/ghostipedia/terminacore/mixin/TFCMetalMixin.java
@@ -3,6 +3,7 @@ package com.ghostipedia.terminacore.mixin;
 import net.dries007.tfc.util.Metal;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(Metal.class)
 public class TFCMetalMixin {
